67c182178ea9ef91fa9c11a01597cc7213b514dc
97Ban <moetibanele5@gmail.com>
2022-06-07 14:03:54 -0700

    My commit message

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+*main-Prints out
+*Return: Always 0.
+*/
+int main(void)
+{
+_putchar('_');
+_putchar('put');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+ return (0);
+}

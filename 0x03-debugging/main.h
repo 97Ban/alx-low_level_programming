6356e72a9f80101517a228ee50cc7273e1253705
97Ban <moetibanele5@gmail.com>
2022-06-09 06:11:26 -0700

    task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,5 +3,7 @@
 
 #include <stdio.h>
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
+int largest_number(int a, int b, int b);
+void print_remaining_days(int month, int day, int year);
+int largest_day(int month, int day);
 #endif
